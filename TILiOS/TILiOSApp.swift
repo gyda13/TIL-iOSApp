2389ff681e59275506fddf1095bcd5c7e8757091
gyda13 <gyda2100@gmail.com>
2023-02-16 03:03:20 +0300

    Initial Commit

---
+++ TILiOS/TILiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILiOSApp.swift
+//  TILiOS
+//
+//  Created by gyda almohaimeed on 25/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TILiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
